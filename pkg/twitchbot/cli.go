f1b95e1e0cde8914714369bc74872e58304e3381
Dan <dan@aredherring.tech>
2025-02-17 20:04:00 -0800

    Add utility to look up a broadcaster ID

--- pkg/twitchbot/cli.go
+++ pkg/twitchbot/cli.go
@@ -2,8 +2,11 @@ package twitchbot
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"os"
 
+	"example.com/twitchbot/pkg/twitch"
 	"github.com/urfave/cli/v3"
 )
 
@@ -15,9 +18,54 @@ var Root = cli.Command{
 				return serve(ctx)
 			},
 		},
+		{
+			Name: "util",
+			Commands: []*cli.Command{
+				{
+					Name: "lookup-broadcaster-id",
+					Arguments: []cli.Argument{
+						&cli.StringArg{
+							Name: "broadcaster_id",
+							Max:  -1,
+						},
+					},
+					Action: lookupBroadcasterByLoginName,
+				},
+			},
+		},
 	},
 }
 
 func Run(ctx context.Context) error {
 	return Root.Run(ctx, os.Args)
+}
+
+func lookupBroadcasterByLoginName(ctx context.Context, c *cli.Command) error {
+	cfg := initTwitchConfig([]string{"user:read:chat"})
+	token, err := fetchTokenFromKeyring()
+	if err != nil {
+		token, err = fetchTokenFromTwitch(ctx, cfg)
+		if err != nil {
+			return err
+		}
+	}
+
+	client := twitch.New(cfg, token)
+	users, err := client.Users(ctx, &twitch.UsersRequest{
+		Login: c.Args().Slice(),
+	})
+
+	if err != nil {
+		return fmt.Errorf("fetch users: %w", err)
+	}
+
+	for _, user := range users.Data {
+		fmt.Fprintf(c.Writer, "%s\n", user.ID)
+	}
+
+	if err := saveTokenInKeyring(token); err != nil {
+		// Print a warning but don't die
+		log.Printf("could not save token to OS keyring: %s", err)
+	}
+	return nil
 }
